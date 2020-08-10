7fbbf30bfc219f6c5590fc68ba652410d7c6185e
jeongsooyoung <code.ameba@gmail.com>
2020-08-10 19:55:09 +0900

    redux on vanilla js

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { createStore } from 'redux';
+
+const add = document.querySelector('#add');
+const minus = document.querySelector('#minus');
+const number = document.querySelector('span');
+
+number.innerText = 0;
+// reducer
+const countModifier = (count = 0, action) => {
+	if (action.type === 'ADD') {
+		return count + 1;
+	} else if (action.type === 'MINUS') {
+		return count - 1;
+	} else {
+		return count;
+	}
+};
+
+// store
+const countStore = createStore(countModifier);
+const onChange = () => {
+	number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+	countStore.dispatch({ type: 'ADD' });
+};
+
+const handleMinus = () => {
+	countStore.dispatch({ type: 'MINUS' });
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
